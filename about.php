965f3cbc9bf4939d0d5605cb70981ae3ba89034e
Monika Kumari Mandal <63465332+missmonikamandal@users.noreply.github.com>
2022-07-18 23:40:30 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,93 @@
+<?php 
+session_start();
+include('includes/config.php');
+error_reporting(0);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Car Rental | About Us</title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="" name="keywords">
+  <meta content="" name="description">
+  <meta content="Author" name="WebThemez">
+  <!-- Favicons -->
+  <link href="img/favicon.png" rel="icon">
+  <link href="img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Raleway:300,400,500,700,800|Montserrat:300,400,700" rel="stylesheet">
+
+  <!-- Bootstrap CSS File -->
+  <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Libraries CSS Files -->
+  <link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <link href="lib/animate/animate.min.css" rel="stylesheet">
+  <link href="lib/ionicons/css/ionicons.min.css" rel="stylesheet">
+  <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="lib/magnific-popup/magnific-popup.css" rel="stylesheet">
+  <link href="lib/ionicons/css/ionicons.min.css" rel="stylesheet">
+
+  <!-- Main Stylesheet File -->
+  <link href="css/style.css" rel="stylesheet"> 
+</head>
+
+<body id="body"> 
+ <?php include('includes/header.php');?>
+ <section id="innerBanner"> 
+  <div class="inner-content">
+    <h2><span>About Us</span><br>We provide high quality cars</h2>
+    <div> 
+    </div>
+  </div> 
+</section><!-- #Page Banner -->
+
+<main id="main">
+      <section id="about" class="wow fadeInUp">
+        <div class="container"> 
+          <div class="row">
+            <div class="col-lg-6 about-img">
+              <img src="img/nissan.jpg" alt="">
+            </div>
+
+            <div class="col-lg-6 content">
+              <h2>Get a completely sanitized car at your doorstep with minimal human interaction.</h2>
+              <h3>We offer the fastest and easiest way to book our self-drive car rental service.
+                 Planning to visit your hometown, emergency travel, going out to holiday destinations, or weekend road trips, carRental is always there to provide you with a safer mode of transport across 45+ cities.</h3>
+              <p>We simplified car rentals, so you can focus on what's important to you.</p>
+              <ul>
+                <li><i class="icon ion-ios-checkmark-outline"></i> Fuel Cost Included: Don’t worry about mileage! All fuel costs are included. If you refill fuel, we’ll pay you back!</li>
+                <li><i class="icon ion-ios-checkmark-outline"></i> No Hidden Charges: Our prices include taxes and insurance.What you see is what you really pay!</li>
+                <li><i class="icon ion-ios-checkmark-outline"></i> Go Anywhere: Our cars have all-India permits.Just remember to pay state tolls and entry taxes.</li>
+                <li><i class="icon ion-ios-checkmark-outline"></i> 24x7 Roadside Assistance: We have round-the-clock, pan India partners. Help is never far away from you. </li>
+              </ul> 
+            </div>
+          </div>
+
+        </div>
+      </section><!-- #about -->
+    </main>
+    <?php include('includes/footer.php');?>
+
+    <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+
+    <!-- JavaScript  -->
+    <script src="lib/jquery/jquery.min.js"></script>
+    <script src="lib/jquery/jquery-migrate.min.js"></script>
+    <script src="lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/superfish/hoverIntent.js"></script>
+    <script src="lib/superfish/superfish.min.js"></script>
+    <script src="lib/wow/wow.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="lib/magnific-popup/magnific-popup.min.js"></script>
+    <script src="lib/sticky/sticky.js"></script> 
+    <script src="contact/jqBootstrapValidation.js"></script>
+    <script src="contact/contact_me.js"></script>
+    <script src="js/main.js"></script>
+
+  </body>
+  </html>
